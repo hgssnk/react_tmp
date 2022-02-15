d8f44049a2ff0084918d6041d0e6d02ce1342866
hgssnk <hgssnk@gmail.com>
2022-02-15 23:04:38 +0900

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# react_tmp
+
+git clone https://github.com/hgssnk/react_tmp.github
+
+cd react_tmp
+
+yarn start
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="hoge">
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default App;
